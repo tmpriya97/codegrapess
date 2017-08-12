43bf4ed735fdc855e2d8796f1110a09c8fb9a812
tmpriya97 <30309090+tmpriya97@users.noreply.github.com>
2017-08-12 16:28:46 +0530

    odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int number;
+scanf("%d",&number);
+if((n>1)&&(number!='\0'))
+{
+if(number%2==0)
+printf("number%d is even",number);
+else
+printf("number%d is odd",number);
+}
+else
+  printf("not applicable");
+}
