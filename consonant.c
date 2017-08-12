7c2dcdcd461c17a324503a2ecbf1e9c25a56bcfd
tmpriya97 <30309090+tmpriya97@users.noreply.github.com>
2017-08-12 17:01:42 +0530

    Create consonant.c

---
+++ consonant.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char alpha;
+scanf("%c",&alpha);
+if((alpha=='a')||(alpha=='e')||(alpha=='i')||(alpha=='o')||(alpha=='u')||(alpha=='A')||(alpha=='E')||I||O||U)))
+    printf("%c is a vowel",alpha);
+else
+    printf("%c is a consonant",alpha);
+}
